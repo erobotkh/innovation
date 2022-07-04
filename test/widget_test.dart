ea6270af3bd5ae2e647512669f7f7efe284e3301
Thea Choem <theacheng.g6@gmail.com>
2022-07-04 11:23:22 +0700

    chore: basic project cleaning & setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:innovation/main.dart';
+import 'package:innovation/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
