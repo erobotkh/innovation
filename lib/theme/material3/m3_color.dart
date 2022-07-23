95d32083f9108f2bb421aef5e625a0404d78128d
Thea Choem <29684683+theachoem@users.noreply.github.com>
2022-07-23 19:10:59 +0700

    close #8 adaptive home design (#9)

--- lib/theme/material3/m3_color.dart
+++ lib/theme/material3/m3_color.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:innovation/theme/theme_constant.dart';
 
 export '../extensions/color_extension.dart';
 export '../extensions/color_scheme_extension.dart';
@@ -7,6 +8,11 @@ class M3Color {
   /// Use `M3Color.of(context)` instead of `Theme.of(context).colorScheme`
   static ColorScheme of(BuildContext context) {
     return Theme.of(context).colorScheme;
+  }
+
+  static Map<int, Color> dayColorsOf(BuildContext context) {
+    bool isDarkMode = Theme.of(context).colorScheme.brightness == Brightness.dark;
+    return isDarkMode ? ThemeConstant.colorsByDayDark : ThemeConstant.colorsByDayLight;
   }
 
   static Brightness keyboardAppearance(BuildContext context) {
--- lib/views/home/home_view_model.dart
+++ lib/views/home/home_view_model.dart
@@ -1,3 +1,25 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
 import 'package:innovation/core/base/base_view_model.dart';
 
-class HomeViewModel extends BaseViewModel {}
+class HomeViewModel extends BaseViewModel {
+  late final ValueNotifier<double?> minimumHeightNotifier;
+
+  HomeViewModel() {
+    minimumHeightNotifier = ValueNotifier<double?>(null);
+  }
+
+  @override
+  void dispose() {
+    minimumHeightNotifier.dispose();
+    super.dispose();
+  }
+
+  void setCardSize(double height) {
+    double nextValue = min(height, minimumHeightNotifier.value ?? height);
+    if (nextValue != minimumHeightNotifier.value) {
+      minimumHeightNotifier.value = nextValue;
+    }
+  }
+}
